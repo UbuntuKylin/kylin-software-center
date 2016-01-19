06c9999acfd760bd46b17fa0ef1d9712fef5fbc2
wenbo <wenbo@kylinos.cn>
2016-01-19 15:54:51 +0800

    Release 1.3.7

--- models/globals.py
+++ models/globals.py
@@ -44,7 +44,7 @@ class Globals:
     LAUNCH_MODE = 'quiet'
 
     # uksc version number
-    UKSC_VERSION = '1.3.6'
+    UKSC_VERSION = '1.3.7'
 
     # open with local deb file
     LOCAL_DEB_FILE = None
